275bc0556e01b34d0e56bb36bf7ed2b930a906af
jeremykdev11 <jeremykdev11@gmail.com>
2024-11-26 00:20:53 -0500

    Circle Blast! - Part 2

--- circle-blast/js/classes.js
+++ circle-blast/js/classes.js
@@ -7,3 +7,30 @@ class Ship extends PIXI.Sprite {
         this.y = y;
     }
 }
+
+class Circle extends PIXI.Graphics {
+    constructor(radius, color = 0xff0000, x = 0, y = 0) {
+        super();
+        this.circle(x, y, radius);
+        this.fill(color);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.fwd = getRandomUnitVector();
+        this.speed = 50;
+        this.isAlive = true;
+    }
+
+    move(dt = 1 / 60) {
+        this.x += this.fwd.x * this.speed * dt;
+        this.y += this.fwd.y * this.speed * dt;
+    }
+
+    reflectX() {
+        this.fwd.x *= -1;
+    }
+
+    reflectY() {
+        this.fwd.y *= -1;
+    }
+}
